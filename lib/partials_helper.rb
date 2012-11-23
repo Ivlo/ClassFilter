3786d4e5df3d068ea778888526a20476627a2190
Sergio Ramirez <sirtdevelopment@gmail.com>
2012-11-23 13:18:32 +0100

    Corrigiendo la función module

--- lib/partials_helper.rb
+++ lib/partials_helper.rb
@@ -3,7 +3,7 @@ module PartialsHelper
     partial "layouts/partials/shared/#{partial_name.to_s}", :locals=>locals
   end
 
-  def module partial_name, locals = {}
+  def modules partial_name, locals = {}
     partial "layouts/partials/modules/#{partial_name.to_s}", :locals=>locals
   end
 end
