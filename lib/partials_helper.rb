7cf72fbb95f13203c0fd077fdf94d7cc986452e3
Sergio Ramirez <sergio.ramirez@cocktail.com>
2013-08-27 09:36:19 +0200

    modules y shared aceptan bloques

--- lib/partials_helper.rb
+++ lib/partials_helper.rb
@@ -1,9 +1,20 @@
 module PartialsHelper
-	def shared partial_name, locals = {}
-    partial "layouts/partials/shared/#{partial_name.to_s}", :locals=>locals
+	def shared partial_name, locals = {}, &block
+    render_with_block "layouts/partials/shared/#{partial_name.to_s}", locals, &block
   end
 
-  def modules partial_name, locals = {}
-    partial "layouts/partials/modules/#{partial_name.to_s}", :locals=>locals
+  def modules(partial_name, locals = {}, &block) 
+    render_with_block "layouts/partials/modules/#{partial_name.to_s}", locals, &block
+  end
+
+  def render_with_block partial_path, locals = {}, &block
+    if block_given?
+      old_buffer, @_out_buf = @_out_buf, ''
+      _yield = yield
+      output = partial partial_path, :locals => { :_yield => _yield }.merge(locals)
+      @_out_buf = old_buffer + output
+    else
+      partial partial_path, :locals=>locals
+    end
   end
 end
